c87f83d7e8745f1ef3b493a6cb701bf9a490b4f3
Neil Thompson <neil@white-label-development.co.uk>
2012-10-19 09:55:09 +0100

    Added downloadable PDF CV
    added 'Working software that matters' text

---
+++ WLD-Web/WLD-Web/Controllers/DocumentController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WLD_Web.Controllers
+{
+    public class DocumentController : Controller
+    {
+        //
+        // GET: /Document/CurriculumVitae
+
+        public ActionResult CurriculumVitae()
+        {
+            string contentType = "application/pdf";
+            string fileLocation = Server.MapPath("~/Documents/thompson_cv.pdf");
+            return File(fileLocation, contentType, "thompson_cv.pdf");
+        }
+
+    }
+}
